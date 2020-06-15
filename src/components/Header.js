6aa6f63f0d090748628ad2099a8b182cb1910671
Venky <>
2020-06-15 01:13:17 -0500

    filtering and subscriptions

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,9 +10,9 @@ class Header extends Component {
             <div className="flex pa1 justify-between nowrap orange">
                 <div className="flex flex-fixed black">
                     <div className="fw7 mr1">Hacker News</div>
-                    <Link to="/" className="ml1 no-underline black">
-                        new
-              </Link>
+                    <Link to="/" className="ml1 no-underline black">new</Link>
+                    <div className="ml1">|</div>
+                    <Link to="/search" className="ml1 no-underline black">search</Link>
                     {authToken && (
                         <div className="flex">
                             <div className="ml1">|</div>
